17e186abbd5fd7736a851fd2a0a051fc7dbc7689
Guilherme Marques <gui.ams@hotmail.com>
2015-09-09 17:42:08 -0300

    add permissions

--- api_rest/lawsuits/views.py
+++ api_rest/lawsuits/views.py
@@ -1,21 +1,25 @@
 from django.shortcuts import render
 from lawsuits.models import Lawsuit
 from lawsuits.serializers import LawsuitSerializer, UserSerializer
-from rest_framework import generics
+from rest_framework import generics, permissions
 from django.contrib.auth.models import User
+from lawsuits.permissions import IsOwnerOrReadOnly
 
 
 class LawsuitList(generics.ListCreateAPIView):
     queryset = Lawsuit.objects.all()
     serializer_class = LawsuitSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
-    # def perform_create(self, serializer):
-    # 	serializer.save(user=self.request.user)
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
 
 class LawsuitDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Lawsuit.objects.all()
     serializer_class = LawsuitSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                      IsOwnerOrReadOnly,)
 
 
 class UserList(generics.ListAPIView):
